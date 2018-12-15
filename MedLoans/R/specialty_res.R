032902a2fbca24ef350f423f806849aafc919dcf
Kevin Chen <kevin_chen1@brown.edu>
2018-12-15 17:29:42 -0500

    rewrite specialty functions

--- MedLoans/R/specialty_res.R
+++ MedLoans/R/specialty_res.R
@@ -8,8 +8,34 @@
 #' specialty_res()
 
 specialty_res <- function(specialty) {
-  res <- specialty_info %>%
-    filter(Specialty == specialty) %>%
-    select(`Years of Training`)
-  print(as.numeric(res))
+  if(specialty == "Anesthesiology") {res = 4}
+  if(specialty == "Dermatology") {res = 4}
+  if(specialty == "Emergency Medicine") {res = 3}
+  if(specialty == "Family Practice") {res = 3}
+  if(specialty == "General Surgery") {res = 5}
+  if(specialty == "Internal Medicine") {res = 3}
+  if(specialty == "Neurology") {res = 4}
+  if(specialty == "Neurosurgery") {res = 7}
+  if(specialty == "Obstetrics/Gynecology") {res = 4}
+  if(specialty == "Ophthalmology") {res = 4}
+  if(specialty == "Orthopedic Surgery") {res = 5}
+  if(specialty == "Otolaryngology") {res = 5}
+  if(specialty == "Pathology") {res = 4}
+  if(specialty == "Pediatrics") {res = 3}
+  if(specialty == "Physical Medicine") {res = 4}
+  if(specialty == "Plastic Surgery") {res = 6}
+  if(specialty == "Psychiatry") {res = 4}
+  if(specialty == "Radiation Oncology") {res = 5}
+  if(specialty == "Radiology, Diagnostic") {res = 5}
+  if(specialty == "Urology") {res = 5}
+  if(specialty == "Cardiology") {res = 6}
+  if(specialty == "Endocrinology") {res = 5}
+  if(specialty == "Gastroenterology") {res = 6}
+  if(specialty == "Infectious Disease") {res = 5}
+  if(specialty == "Nephrology") {res = 5}
+  if(specialty == "Oncology") {res = 5}
+  if(specialty == "Pulmonary") {res = 5}
+  if(specialty == "Rheumatology") {res = 5}
+  if(specialty == "Immunology") {res = 5}
+  print(res)
 }
--- MedLoans/R/specialty_salary.R
+++ MedLoans/R/specialty_salary.R
@@ -8,8 +8,34 @@
 #' specialty_salary()
 
 specialty_salary <- function(specialty) {
-  salary <- specialty_info %>%
-    filter(Specialty == specialty) %>%
-    select(`Annual Salary`)
-  print(as.numeric(salary))
+  if(specialty == "Anesthesiology") {salary = 386000}
+  if(specialty == "Dermatology") {salary = 392000}
+  if(specialty == "Emergency Medicine") {salary = 350000}
+  if(specialty == "Family Practice") {salary = 219000}
+  if(specialty == "General Surgery") {salary = 322000}
+  if(specialty == "Internal Medicine") {salary = 230000}
+  if(specialty == "Neurology") {salary = 244000}
+  if(specialty == "Neurosurgery") {salary = 663000}
+  if(specialty == "Obstetrics/Gynecology") {salary = 300000}
+  if(specialty == "Ophthalmology") {salary = 357000}
+  if(specialty == "Orthopedic Surgery") {salary = 497000}
+  if(specialty == "Otolaryngology") {salary = 383000}
+  if(specialty == "Pathology") {salary = 286000}
+  if(specialty == "Pediatrics") {salary = 212000}
+  if(specialty == "Physical Medicine") {salary = 269000}
+  if(specialty == "Plastic Surgery") {salary = 501000}
+  if(specialty == "Psychiatry") {salary = 273000}
+  if(specialty == "Radiation Oncology") {salary = 468000}
+  if(specialty == "Radiology, Diagnostic") {salary = 401000}
+  if(specialty == "Urology") {salary = 373000}
+  if(specialty == "Cardiology") {salary = 423000}
+  if(specialty == "Endocrinology") {salary = 212000}
+  if(specialty == "Gastroenterology") {salary = 408000}
+  if(specialty == "Infectious Disease") {salary = 231000}
+  if(specialty == "Nephrology") {salary = 294000}
+  if(specialty == "Oncology") {salary = 363000}
+  if(specialty == "Pulmonary") {salary = 321000}
+  if(specialty == "Rheumatology") {salary = 257000}
+  if(specialty == "Immunology") {salary = 272000}
+  print(salary)
 }
